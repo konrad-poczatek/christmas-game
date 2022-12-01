1c28d1cbd7ebcbde333b3b3265ac80cc416a9605
arturo <arturo.rodriguez@leovegas.com>
2022-12-01 09:42:19 -0300

    add java solution 1

--- 1/solution.java
+++ 1/solution.java
@@ -1,5 +1,13 @@
 class Solution {
     public boolean isPalindrome(int number) {
-        
+        if(number == 0) return true;
+        else if(number < 0) return false;
+        else return checknumber(number);
+    }
+
+    static boolean checknumber(int number){
+        String numberString = String.valueOf(number);
+        StringBuilder numberStringBackWards = new StringBuilder(numberString).reverse();
+        return numberString.equals(numberStringBackWards.toString());
     }
 }
