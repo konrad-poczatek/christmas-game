3f5ee514670043420079228038e5abd37d1a779e
Kumar <vipin.kumar@leovegas.com>
2022-12-01 17:14:54 +0000

    Solution for 1

--- 1/solution.java
+++ 1/solution.java
@@ -1,5 +1,27 @@
-class Solution {
+class solution {
     public boolean isPalindrome(int number) {
-        
+        if (number < 0) {
+            return false;
+        } else if (number < 10){
+            return true;
+        } else {
+            int lengthMinusOne = (int)Math.floor(Math.log10(number));
+            return areEndDigitsSame(number, lengthMinusOne);
+        }
+    }
+
+    private boolean areEndDigitsSame(int number, int lengthMinusOne) {
+        int firstDigit = number/(int)Math.pow(10, lengthMinusOne);
+        int lastDigit = number%10;
+        if(firstDigit != lastDigit) {
+            return false;
+        } else {
+            if (lengthMinusOne > 2) {
+                int nextNumber = ((int)(number - firstDigit * Math.pow(10, lengthMinusOne)))/10;
+                areEndDigitsSame(nextNumber, lengthMinusOne -2);
+            }
+
+        }
+        return true;
     }
 }
