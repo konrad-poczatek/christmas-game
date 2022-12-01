641f09576a03724b90ab9d2999b189828066e3e0
andrey.yelmanov <andrey.yelmanov@leovegas.com>
2022-12-01 16:45:51 +0100

    My solution

--- 1/solution.java
+++ 1/solution.java
@@ -1,5 +1,18 @@
 class Solution {
     public boolean isPalindrome(int number) {
-        
+        if (number < 0) {
+            return false;
+        }
+
+        int original = number;
+        int rem;
+        int reverse = 0;
+        while (number != 0) {
+            rem = number % 10;
+            reverse = reverse * 10 + rem;
+            number = number / 10;
+        }
+
+       return original == reverse;
     }
 }
