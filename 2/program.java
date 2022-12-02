998e96a822cffe9cf2dbf76cbe0e3f9435814607
Kumar <vipin.kumar@leovegas.com>
2022-12-02 16:31:00 +0000

    solution of 2nd

--- 2/program.java
+++ 2/program.java
@@ -1,5 +1,21 @@
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Stack;
+
 class Program {
+  private static Map<Character, Character> complementMap = new HashMap<>(){{put('{','}');put('(',')');put('[',']');}};
   public boolean areBracketsValid(String brackets) {
+    Stack<Character> characterStack = new Stack<>();
+    for(Character character : brackets.toCharArray()) {
+      if(complementMap.containsKey(character)) {
+        characterStack.push(character);
+      } else if (characterStack.size() > 0 && complementMap.get(characterStack.peek()).equals(character)) {
+        characterStack.pop();
+      }
+    }
+    if(characterStack.isEmpty()) {
+      return true;
+    }
     return false;
   }
 }
--- 2/program.kt
+++
@@ -1,5 +0,0 @@
-class Program {
-  fun areBracketsValid(brackets: String): Boolean {
-    return false
-  }
-}
