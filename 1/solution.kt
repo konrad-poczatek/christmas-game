3f8c7ab2666c0c262dd680ad0a953f2afd10a94c
Pierre Alvarez Gonzalez <90254173+pierrealvarez@users.noreply.github.com>
2022-12-02 09:53:27 +0100

    Day 1

--- 1/solution.kt
+++ 1/solution.kt
@@ -1,5 +1,18 @@
 class Solution {
     fun isPalindrome(number: Int): Boolean {
-        
+        val numberToCharList = number.toString().toList()
+        val numberToListSize = numberToCharList.size
+
+        return if (numberToListSize % 2 == 0) {
+            val splitRight = numberToCharList.subList(0, numberToListSize / 2)
+            val splitLeft = numberToCharList.subList(numberToListSize / 2, numberToListSize)
+
+            splitRight == splitLeft.reversed()
+        } else {
+            val splitRight = numberToCharList.subList(0, numberToListSize / 2)
+            val splitLeft = numberToCharList.subList(numberToListSize / 2 + 1, numberToListSize)
+
+            splitRight == splitLeft.reversed() && numberToListSize != 1
+        }
     }
 }
