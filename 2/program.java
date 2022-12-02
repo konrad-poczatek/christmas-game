f2354df9a468b591093a148704b30428e6099f65
Alexandre Le Foll <110401521+alexandrlefoll@users.noreply.github.com>
2022-12-02 11:36:53 +0000

    Update program.java
    Added recursive approach :D

--- 2/program.java
+++ 2/program.java
@@ -1,5 +1,22 @@
 class Program {
   public boolean areBracketsValid(String brackets) {
-    return false;
+    Map<String, String> matches = new HashMap<>();
+        matches.put("[", "]");
+        matches.put("{", "}");
+        matches.put("(", ")");
+
+        List<String> split = Arrays.stream(brackets.split("")).toList();
+
+        if (matches.containsValue(split.get(0)) || split.size() % 2 != 0) return false;
+
+        for(int i=0; i<split.size(); i++) {
+            int indexOfMatch = split.indexOf(matches.get(split.get(i)));
+            if (indexOfMatch-(i+1)>0 && (indexOfMatch-(i+1)) % 2 == 0) {
+                if(!areBracketsValid(brackets.substring(i + 1, indexOfMatch))) {
+                    return false;
+                }
+            }
+        }
+        return true;
   }
 }
