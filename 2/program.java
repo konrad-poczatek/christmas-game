33a39e2ad19a49d08d34eb9adaf67ff0865da43e
andrey.yelmanov <andrey.yelmanov@leovegas.com>
2022-12-02 15:22:12 +0100

    Solution with Shunting yard algorithm

--- 2/program.java
+++ 2/program.java
@@ -1,5 +1,45 @@
 class Program {
   public boolean areBracketsValid(String brackets) {
+    char[] chars = brackets.toCharArray();
+
+    Stack<Character> stack = new Stack<>();
+    int i = 0;
+    while (i < chars.length) {
+      switch (chars[i]) {
+        case '{' -> stack.push(chars[i]);
+        case '}'-> {
+          boolean check = checkBracket(stack, '{');
+          if (!check && stack.empty()){
+            return false;
+          }
+        }
+        case '(' -> stack.push(chars[i]);
+        case ')'-> {
+          boolean check = checkBracket(stack, '(');
+          if (!check && stack.empty()){
+            return false;
+          }
+        }
+        case '[' -> stack.push(chars[i]);
+        case ']'-> {
+          boolean check = checkBracket(stack, '[');
+          if (!check && stack.empty()){
+            return false;
+          }
+        }
+      }
+      i++;
+    }
+    return true;
+  }
+
+  private boolean checkBracket(Stack<Character> stack, char openBracket){
+    while (!stack.empty()){
+      Character current =stack.pop();
+      if(current == openBracket){
+        return true;
+      }
+    }
     return false;
   }
 }
