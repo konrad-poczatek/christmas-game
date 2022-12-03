cde3022ae682b5bb1d4ca2be13a1adff69d9a9e7
Pooja-Gulshetty <67377292+Pooja-Gulshetty@users.noreply.github.com>
2022-12-03 22:23:59 +0100

    Two sum solution.

--- 0/solution.kt
+++ 0/solution.kt
@@ -1,11 +1,18 @@
 class Solution {
+
     fun twoSum(gifts: IntArray, maxCost: Int): IntArray {
-        val map = hashMapOf<Int, Int>()
-        gifts.forEachIndexed { index, i ->
-            val goal = maxCost - i
-            if (map[goal] != null) return intArrayOf(map[goal]!!, index)
-            map[i] = index
+        val alreadyVisitedNumbers = mutableMapOf<Int, Int>()
+        for ((position, gift) in gifts.withIndex()) {
+            val remaining = maxCost - gift
+            if (alreadyVisitedNumbers.containsKey(remaining)) {
+                val firstItemIndex = alreadyVisitedNumbers.get(remaining)!!
+                return intArrayOf(firstItemIndex, position)
+            }
+
+            alreadyVisitedNumbers.put(gift, position)
         }
-        return intArrayOf(-1, -1)
+
+        return IntArray(2)
     }
+
 }
