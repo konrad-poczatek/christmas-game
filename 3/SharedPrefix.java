3876c8fa25b446497f2a686d2cbfc5163abfb58f
Kumar <vipin.kumar@leovegas.com>
2022-12-03 13:27:35 +0000

    solution of 3rd problem

--- 3/SharedPrefix.java
+++ 3/SharedPrefix.java
@@ -1,5 +1,20 @@
 class SharedPrefix {
-  public String biggestSharedPrefix(String[] gifts) {
-    
-  }
+    public String biggestSharedPrefix(String[] gifts) {
+        String highestOccurancePrefix = "";
+        int characterIndex = 0;
+        boolean prefixMatched = true;
+        while (characterIndex < gifts[0].length() && prefixMatched) {
+            for (String gift : gifts) {
+                if (!gift.startsWith(gifts[0].substring(0, characterIndex + 1))) {
+                    prefixMatched = false;
+                    break;
+                }
+            }
+            if (prefixMatched) {
+                highestOccurancePrefix = gifts[0].substring(0, characterIndex + 1);
+                characterIndex++;
+            }
+        }
+        return highestOccurancePrefix;
+    }
 }
