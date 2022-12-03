0572f05d5c38451fea04905507bd80d9091cba9d
arturo <arturo.rodriguez@leovegas.com>
2022-12-03 20:15:11 -0300

    3

--- 3/SharedPrefix.java
+++ 3/SharedPrefix.java
@@ -1,5 +1,18 @@
 class SharedPrefix {
   public String biggestSharedPrefix(String[] gifts) {
-    
+    int counterOrder = 0;
+    boolean flag = true;
+    StringBuilder firstLetters = new StringBuilder(list[0].charAt(counterOrder));
+    while (flag) {
+      for (String s : list) {
+        if (!s.startsWith(firstLetters.toString())) flag = false;
+        if (!flag) break;
+      }
+      if (flag){
+        counterOrder++;
+        firstLetters.append(list[0].charAt(counterOrder));
+      }else break;
+    }
+    return (counterOrder==0)? "" : firstLetters.toString();
   }
 }
