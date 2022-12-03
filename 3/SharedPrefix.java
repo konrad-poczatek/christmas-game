3e059850da8e40a0ae509ac8e1bc6a4ccde40843
Andrey Yelmanov <112406627+AndreyYelmanov@users.noreply.github.com>
2022-12-03 20:57:43 +0000

    Solution of 3 task. The worst solution award

--- 3/SharedPrefix.java
+++ 3/SharedPrefix.java
@@ -1,5 +1,26 @@
 class SharedPrefix {
   public String biggestSharedPrefix(String[] gifts) {
-    
+    Map<String, Integer> words = new HashMap<>();
+        int j=0;
+        while (j< gifts.length) {
+            int i = 0;
+            while (i < gifts.length) {
+                final String prefix = gifts[i].substring(0, j);
+                int count = 0;
+                for (String gift : gifts) {
+                    if (gift.substring(0, j).equals(prefix)) {
+                        count++;
+                    }
+                }
+                words.put(gifts[i].substring(0,j), count);
+                i++;
+            }
+            j++;
+        }
+        final List<Map.Entry<String, Integer>> collect = words.entrySet().stream()
+                .sorted(Map.Entry.comparingByValue(Comparator.reverseOrder()))
+                .filter(stringIntegerEntry -> !stringIntegerEntry.getKey().isBlank() && stringIntegerEntry.getKey().length() != 1)
+                .collect(Collectors.toUnmodifiableList());
+        return collect.get(0).getKey();
   }
 }
