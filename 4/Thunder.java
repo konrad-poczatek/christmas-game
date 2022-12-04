bc43563ff1ed71b062fd578419daee416aad7a84
andrey.yelmanov <andrey.yelmanov@leovegas.com>
2022-12-04 22:02:15 +0100

    Solution of 4 task. ready

--- 4/Thunder.java
+++ 4/Thunder.java
@@ -1,5 +1,5 @@
 class Thunder {
-    public static String solve(String phrase, int height) {
+    public String solve(String phrase, int height) {
         if (phrase.length() == 0 || height == 0) {
             return "";
         }
@@ -10,28 +10,28 @@ class Thunder {
         final char[] chars = phrase.toCharArray();
         StringBuilder thunderString = new StringBuilder();
         int groupCounter = 1;
-
+        final int hOffset = 2 * height - 2;
 
         for (int i = 0; i < height; i++) {
-            int groupCounter2 = 1;
             for (int j = 0, k = 1; j < groups * height; j++, k++) {
                 if (k == height) {
                     k = 1;
                 }
                 if (j == 0) {
                     thunderString.append(chars[i]);
-                } else if (j == (height - 1) * groupCounter && (height + height - 2) * groupCounter + i < phrase.length()) {
-                    thunderString.append(chars[(height + height - 2) * groupCounter + i]);
-                    groupCounter++;
-                } else if (i != 0 && i != height - 1 && (i + k == height) && (groupCounter * (2 * (height) - 2) - (i - 2) - 2 < phrase.length())) {
-                    thunderString.append(chars[groupCounter * (2 * (height) - 2) - (i - 2) - 2]);
                 } else {
-                    thunderString.append(" ");
+                    if (j == (height - 1) * groupCounter && hOffset * groupCounter + i < phrase.length()) {
+                        thunderString.append(chars[hOffset * groupCounter + i]);
+                        groupCounter++;
+                    } else if (i != 0 && i != height - 1 && (i + k == height) && (groupCounter * hOffset - (i - 2) - 2 < phrase.length())) {
+                        thunderString.append(chars[groupCounter * hOffset - (i - 2) - 2]);
+                    } else {
+                        thunderString.append(" ");
+                    }
                 }
             }
             groupCounter = 1;
             thunderString.append("\n");
-
         }
         return thunderString.toString();
     }
