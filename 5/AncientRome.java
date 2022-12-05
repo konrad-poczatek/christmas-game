d751f8f9f60db3a63b523898ba1766c6ba13ed82
Alexandre Le Foll <110401521+alexandrlefoll@users.noreply.github.com>
2022-12-05 12:14:39 +0000

    Better work this one

--- 5/AncientRome.java
+++ 5/AncientRome.java
@@ -1,5 +1,19 @@
 class AncientRome {
     public int convertToArabicNumber(String romanNumber) {
-        
+        Map<String, Integer> romanNumbersValue = Map.of("I", 1, "V", 5, "X", 10, "L", 50, "C", 100, "D", 500, "M", 1000 );
+        String[] split = romanNumber.split("");
+        List<Integer> counts = new ArrayList<>();
+
+        for(int i=split.length-1; i>=0; i--) {
+            if(counts.size() > 0 && counts.get(counts.size()-1) > romanNumbersValue.get(split[i])) {
+                counts.add(-romanNumbersValue.get(split[i]));
+            } else {
+                counts.add(romanNumbersValue.get(split[i]));
+            }
+        }
+
+        return counts.stream()
+                .mapToInt(Integer::intValue)
+                .sum();
     }
 }
