721e44b9a3547d3d01fbd1a0fa6b3f566866d2a0
Konrad Początek <114927967+konrad-poczatek@users.noreply.github.com>
2022-12-07 08:31:46 +0000

    Create Santonacci.java

--- 7/Santonacci.java
+++ 7/Santonacci.java
@@ -1,14 +1,5 @@
 class Santonacci {
     public int fibonacci(int n) {
-        if (n == 0) return 0;
-        int totalsum = 0;
-        int firstNumber = 0;
-        int secondNumber = 1;
-        for (int i = 0; i < n; i++) {
-            totalsum = firstNumber + secondNumber;
-            firstNumber = secondNumber;
-            secondNumber = totalsum;
-        }
-        return totalsum;
+        
     }
 }
