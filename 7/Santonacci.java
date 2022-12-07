78ca1ea735953d3d228d04409e0f8a3472f45921
arturo <arturo.rodriguez@leovegas.com>
2022-12-07 17:42:24 -0300

    7

--- 7/Santonacci.java
+++ 7/Santonacci.java
@@ -1,5 +1,14 @@
 class Santonacci {
-    public static int fibonacci(int n) {
-
+    public int fibonacci(int n) {
+        if (n == 0) return 0;
+        int totalsum = 0;
+        int firstNumber = 0;
+        int secondNumber = 1;
+        for (int i = 0; i < n; i++) {
+            totalsum = firstNumber + secondNumber;
+            firstNumber = secondNumber;
+            secondNumber = totalsum;
+        }
+        return totalsum;
     }
 }
