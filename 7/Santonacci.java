6c2ca505ee0e7b354ccff4facd5eaeeca2711c86
andrey.yelmanov <andrey.yelmanov@leovegas.com>
2022-12-07 21:44:53 +0100

    Solution 7

--- 7/Santonacci.java
+++ 7/Santonacci.java
@@ -1,12 +1,18 @@
 class Santonacci {
     public static int fibonacci(int n) {
-        int sum = 0;
-        int n1=0;
-        int n2=1;
-        for(int j=1;j<n;++j){
-            sum= n1+n2;
-            n1=n2;
-            n2=sum;
+        if (n == 0) {
+            return 0;
+        }
+
+        int fib = new int[n + 1];
+        fib[0] = 0;
+        fib[1] = 1;
+
+        int sum = fib[0] + fib[1];
+
+        for (int i = 2; i <= n; i++) {
+            fib[i] = fib[i - 1] + fib[i - 2];
+            sum += fib[i];
         }
         return sum;
     }
