b8ea08ae66d281b685b21e08754b16b2b2522f32
Alexandre Le Foll <110401521+alexandrlefoll@users.noreply.github.com>
2022-12-08 11:32:38 +0000

    Half big brain time

--- 7/Santonacci.java
+++ 7/Santonacci.java
@@ -1,13 +1,6 @@
 class Santonacci {
     public static int fibonacci(int n) {
-        int sum = 0;
-        int n1=0;
-        int n2=1;
-        for(int j=1;j<n;++j){
-            sum= n1+n2;
-            n1=n2;
-            n2=sum;
-        }
-        return sum;
+        int[] sequence = {0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144, 233, 377, 610, 987, 1597, 2584, 4181, 6765, 10946, 17711, 28657, 46368, 75025, 121393, 196418, 317811, 514229};
+        return sequence[n];
     }
 }
