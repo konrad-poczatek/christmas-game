706983322ee8758adc6bab51980870ee52fb2622
andrey.yelmanov <andrey.yelmanov@leovegas.com>
2022-12-09 23:01:18 +0100

    Solution 9

--- 9/SquareRoot.java
+++ 9/SquareRoot.java
@@ -1,5 +1,21 @@
 class SquareRoot {
-  public int findSquareRoot(int number) {
-    
-  }
+    public int findSquareRoot(int number) {
+        Long leftBorder = 0L;
+        Long rightBorder = (long) number;
+        Long temp = (long) number;
+        Long middle;
+
+        while (true) {
+            middle = (leftBorder + rightBorder) / 2;
+            if (middle.equals(temp) || middle * middle == number) {
+                return middle.intValue();
+            }
+            if (middle * middle < number) {
+                leftBorder = middle;
+            } else {
+                rightBorder = middle;
+            }
+            temp = middle;
+        }
+    }
 }
