2cfd3eb624620f9738fb7035b5869dbd56c81fe0
andrey.yelmanov <andrey.yelmanov@leovegas.com>
2022-12-10 21:40:15 +0100

    Solution 10

--- 10/Solution.java
+++ 10/Solution.java
@@ -1,5 +1,13 @@
 class Solution {
-  public String calculatePower(String number, int exp) {
-    
-  }
+    public String calculatePower(String number, int exp) {
+        if (exp == 0)) {
+            return "1";
+        }
+        BigDecimal num = new BigDecimal(number);
+        BigDecimal result = num;
+        for (int i = 0; i < exp - 1; i++) {
+            result = result.multiply(num);
+        }
+        return result.toPlainString();
+    }
 }
