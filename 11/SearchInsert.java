97f0f2d765abe386bbec66d82e68f9c0bf7fc84c
andrey.yelmanov <andrey.yelmanov@leovegas.com>
2022-12-11 15:58:43 +0100

    Solution 11

--- 11/SearchInsert.java
+++ 11/SearchInsert.java
@@ -1,5 +1,11 @@
 class SearchInsert {
     public int searchInsert(int[] integers, int target) {
+        for (int i = 0; i < integers.length; i++) {
+            if (integers[i] >= target) {
+                return i;
+            }
+        }
+        return integers.length;
         
     }
 }
