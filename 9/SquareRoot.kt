fe949859c2435a073a327dc8849b09d17eeefb38
Tom Heijltjes <tom.heijltjes@leovegas.com>
2022-12-12 11:33:43 +0100

    9 - solution

--- 9/SquareRoot.kt
+++ 9/SquareRoot.kt
@@ -1,5 +1,17 @@
 class SquareRoot {
   fun findSquareRoot(number: Int): Int {
-    
+    var tmp1 = number / 2 // divide input
+    var tmp2: Int
+
+    do {
+      tmp2 = tmp1 // asign second var
+      tmp1 = (tmp2 + (number / tmp2)) / 2 // divide original number by divided original number, then sum up with original number and divide by 2
+
+    } while((tmp2 - tmp1) != 0) // as long as we dont have the original number, continue
+
+    return tmp1
+
+    // not allowed function result:
+    //return Math.sqrt(number.toDouble()).toInt()
   }
 }
--- 9/SquareRootTest.kt
+++ 9/SquareRootTest.kt
@@ -2,18 +2,18 @@ import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.Assertions.assertEquals
 
 class SquareRootTest {
-    private var longestSubstring: LongestSubstring = LongestSubstring()
+    private var squareRoot: SquareRoot = SquareRoot()
 
     @Test
-    fun `test longest substring`() {
-        val longestSubstringTestMap = mapOf(
-            "qweqrtw" to 5,
-            "aaaaaa" to 1,
-            "pffew" to 3
+    fun `test square root`() {
+        val squareRootTestMap = mapOf(
+            4 to 2,
+            9 to 3,
+            Integer.MAX_VALUE to 46340
         )
 
-        longestSubstringTestMap.forEach {
-            entry -> assertEquals(entry.value, longestSubstring.lengthOfLongestSubstring(entry.key))
+        squareRootTestMap.forEach {
+            entry -> assertEquals(entry.value, squareRoot.findSquareRoot(entry.key))
         }
     }
 }
