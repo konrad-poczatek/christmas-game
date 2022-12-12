6a1852237271108540570488006f5cddeb810f16
arturo <arturo.rodriguez@leovegas.com>
2022-12-12 17:12:06 -0300

    Revert "Create Thunder.java"
    This reverts commit b1495c9555ac5697261feccf51a5ad86ee010cf5.

--- 4/Thunder.java
+++ 4/Thunder.java
@@ -1,5 +1,17 @@
 class Thunder {
     public String solve(String phrase, int height) {
-        
+        int columns = phrase.length() / 2;
+        if ((columns % 2) != 0) columns += 1;
+        int counter = 1;
+        StringBuilder finalPhrase = new StringBuilder();
+        for (int i = 0; i < height; i++) {
+            for (int j = 0; j < columns; j++) {
+                if (j == 0) finalPhrase.append(phrase.substring(i));
+                else finalPhrase.append(phrase.substring(i + (columns * counter)));
+                counter++;
+            }
+            counter = 1;
+        }
+        return finalPhrase.toString();
     }
 }
