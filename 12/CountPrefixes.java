5a895025295a5eece12dd66ce2c0610da4f27874
Konrad Początek <114927967+konrad-poczatek@users.noreply.github.com>
2022-12-12 10:20:50 +0000

    Create CountPrefixes.java

--- 12/CountPrefixes.java
+++ 12/CountPrefixes.java
@@ -1,7 +1,5 @@
 class CountPrefixes {
     public int countPrefixes(String[] strings, String word) {
-        int counter=0;
-        for (String string : strings) if (word.startsWith(string)) counter++;
-        return counter;
+        
     }
 }
