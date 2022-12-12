444fc5325c7bb9ac33d83e14d6e34d62ad147a36
arturo <arturo.rodriguez@leovegas.com>
2022-12-12 17:30:06 -0300

    12

--- 12/CountPrefixes.java
+++ 12/CountPrefixes.java
@@ -1,5 +1,7 @@
 class CountPrefixes {
     public int countPrefixes(String[] strings, String word) {
-        
+        int counter=0;
+        for (String string : strings) if (word.startsWith(string)) counter++;
+        return counter;
     }
 }
