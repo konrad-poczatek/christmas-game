0b6d03dba1ae408039922e8155dbb8f32bfc0425
andrey.yelmanov <andrey.yelmanov@leovegas.com>
2022-12-13 20:46:18 +0100

    Solution 13

--- 13/StairwaysToHeaven.java
+++ 13/StairwaysToHeaven.java
@@ -1,5 +1,14 @@
 class StairwaysToHeaven {
     public int climbStairs(int x) {
-        return 0;
+        int[] steps = new int[x + 1];
+        steps[0] = 1;
+
+        int combinations = steps[0] + steps[1];
+
+        for (int i = 2; i <= x; i++) {
+            steps[i] = steps[i - 1] + steps[i - 2];
+            combinations += steps[i];
+        }
+        return combinations;
     }
 }
