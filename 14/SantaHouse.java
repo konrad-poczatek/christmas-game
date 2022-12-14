baefc2f14317d9daabc1a4773d178e6c42d3f71f
Konrad Początek <114927967+konrad-poczatek@users.noreply.github.com>
2022-12-14 09:03:19 +0000

    Create SantaHouse.java

--- 14/SantaHouse.java
+++ 14/SantaHouse.java
@@ -1,13 +1,5 @@
 class SantaHouse {
     public int visit(int[] houses) {
-        int number1=0;
-        int number2=0;
-
-        for (int i = 0; i < houses.length; i=i+2) {
-            number1+= houses[i];
-            if (i+1 >= houses.length) break;
-            number2+= houses[i+1];
-        }
-        return Math.max(number1, number2);
+        
     }
 }
