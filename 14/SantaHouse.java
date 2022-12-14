9a290fe00332119073af7d99d74f590e31902fb9
andrey.yelmanov <andrey.yelmanov@leovegas.com>
2022-12-14 20:48:33 +0100

    Solution 14

--- 14/SantaHouse.java
+++ 14/SantaHouse.java
@@ -1,5 +1,20 @@
 class SantaHouse {
     public int visit(int[] houses) {
-        
+        if (houses == null || houses.length == 0) {
+            return 0;
+        }
+        if (houses.length == 1) {
+            return houses[0];
+        }
+        int preMax = houses[0];
+        int max = Integer.max(houses[0], houses[1]);
+
+        for (int i = 2; i < houses.length; i++) {
+            int current = Integer.max(houses[i], Integer.max(max, preMax + houses[i]));
+            preMax = max;
+            max = current;
+        }
+
+        return max;
     }
 }
