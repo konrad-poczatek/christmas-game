52281129fa81fa044b188c12dcbfd15c27ec28d2
arturo <arturo.rodriguez@leovegas.com>
2022-12-15 10:48:27 -0300

    15

--- 15/Triangle.java
+++ 15/Triangle.java
@@ -1,5 +1,26 @@
 class Triangle {
     public List<List<Integer>> createGoodConstruction(int levels) {
-        
+        List<Integer> arraylevel = new ArrayList<>();
+        List<Integer> previousArraylevel = new ArrayList<>();
+        List<List<Integer>> finalList = new ArrayList<>();
+        int number=1;
+        for (int i = 0; i < levels ; i++) {
+            arraylevel.clear();
+            previousArraylevel.clear();
+            if (i>0){
+                previousArraylevel = finalList.get(i-1);
+                for (int j = 0; j <= previousArraylevel.size()+1; j++) {
+                    if (j==0 || j==previousArraylevel.size()+1 ){
+                        number=previousArraylevel.get(0);
+                        arraylevel.add(number);
+                    } else{
+                        number=previousArraylevel.get(j)+previousArraylevel.get(j-1);
+                        arraylevel.add(number);
+                    }
+                }
+            }else arraylevel.add(number);
+            finalList.add(arraylevel);
+        }
+        return finalList;
     }
 }
