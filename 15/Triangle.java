bd5bf39e26e0a36404ede6ec746ba041d333ffcd
andrey.yelmanov <andrey.yelmanov@leovegas.com>
2022-12-15 22:35:57 +0100

    Solution 15

--- 15/Triangle.java
+++ 15/Triangle.java
@@ -1,5 +1,19 @@
 class Triangle {
     public List<List<Integer>> createGoodConstruction(int levels) {
-        
+        List<Integer> raw = new ArrayList<>();
+        List<List<Integer>> raws = new ArrayList<>();
+        int c = 1;
+        for (int i = 0; i < levels; i++) {
+            for (int j = 0; j <= i; j++) {
+                if (j == 0 || i == 0)
+                    c = 1;
+                else
+                    c = c * (i - j + 1) / j;
+                raw.add(c);
+            }
+            raws.add(raw);
+            raw = new ArrayList<>();
+        }
+        return raws;
     }
 }
