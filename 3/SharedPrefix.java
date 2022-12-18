6bf4326e649d347938f31776f2576b613e0b8ebb
sajad.mowlaei68 <sajad.mowlaei@leovegas.com>
2022-12-18 22:50:49 +0100

    Solution for problem 3

--- 3/SharedPrefix.java
+++ 3/SharedPrefix.java
@@ -1,5 +1,24 @@
 class SharedPrefix {
   public String biggestSharedPrefix(String[] gifts) {
-    
+    int numOfGifts = gifts.length;
+
+    if (numOfGifts == 0)
+      return "";
+
+    int shortestGift = 99999999;
+
+    for (int i = 0; i < numOfGifts; ++i)
+      shortestGift = Math.min(shortestGift, gifts[i].length());
+
+    for (int j = 0; j < shortestGift; ++j)
+    {
+      for (int i = 1; i < numOfGifts; ++i)
+      {
+        if (!(gifts[0].charAt(j) == gifts[i].charAt(j)))
+          return gifts[0].substring(0, j);
+      }
+    }
+
+    return gifts[0].substring(0, shortestGift);
   }
 }
