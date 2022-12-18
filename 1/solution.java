8dfad17e8c0d4a78f39b93ef53cda626db91c382
sajad.mowlaei68 <sajad.mowlaei@leovegas.com>
2022-12-18 21:01:43 +0100

    first task solution

--- 1/solution.java
+++ 1/solution.java
@@ -1,5 +1,15 @@
 class Solution {
     public boolean isPalindrome(int number) {
-        
+        if(number<0) return false;
+        else if(number<10) return true;
+        else if(number%10==0) return false;
+        else if(number<100&&number%11==0) return true;
+
+        int revNum = 0, oriNum = number;
+        while (number > 0) {
+            revNum = revNum * 10 + number % 10;
+            number = number / 10;
+        }
+        return revNum == oriNum;
     }
 }
