64e2e9cbf0246e50b9394239330b43d753248258
andrey.yelmanov <andrey.yelmanov@leovegas.com>
2022-12-19 22:06:09 +0100

    Solution 19

--- 17/PatternMatchingChecker.java
+++ 17/PatternMatchingChecker.java
@@ -1,5 +1,17 @@
 class PatternMatchingChecker {
     public boolean isMatch(String str, String pat) {
+        if(pat.equals(str)){
+            return true;
+        }
+        if (pat.equals(".*")){
+            return true;
+        }
+        if (pat.contains("*") && str.length() != 0) {
+            return true;
+        }
+        if(pat.contains(".")){
+
+        }
         return false;
     }
 }
--- 18/OldPhone.java
+++ 18/OldPhone.java
@@ -1,5 +1,36 @@
 class OldPhone {
     public List<String> combinations(String digits) {
-        
+        if (digits == null || digits.length() == 0) {
+            return List.of("\"\"");
+        }
+        List<List<Character>> keypad = List.of(
+                List.of(),
+                List.of(),
+                List.of('a', 'b', 'c'),
+                List.of('d', 'e', 'f'),
+                List.of('g', 'h', 'i'),
+                List.of('j', 'k', 'l'),
+                List.of('m', 'n', 'o'),
+                List.of('p', 'q', 'r', 's'),
+                List.of('t', 'u', 'v'),
+                List.of('w', 'x', 'y', 'z')
+        );
+        char[] keys = digits.toCharArray();
+
+        List<String> comb = new ArrayList<>();
+        for (char ch : keypad.get(Character.getNumericValue(keys[0]))) {
+            comb.add(String.valueOf(ch));
+        }
+
+        for (int i = 1; i < keys.length; i++) {
+            List<String> prevList = new ArrayList<>(comb);
+            comb.clear();
+            for (Character ch : keypad.get(Character.getNumericValue(keys[i]))) {
+                for (String s : prevList) {
+                    comb.add(s + ch);
+                }
+            }
+        }
+        return comb;
     }
 }
