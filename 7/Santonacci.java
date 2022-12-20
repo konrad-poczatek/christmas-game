f55a5b7ececb17640b4b852affb315aacaf482fc
Ramya Vijayakumar <119847883+ramyavijayk@users.noreply.github.com>
2022-12-20 08:23:33 +0100

    Solution for Santonacci

--- 7/Santonacci.java
+++ 7/Santonacci.java
@@ -1,5 +1,19 @@
 class Santonacci {
-    public static int fibonacci(int n) {
-
+    public int fibonacci(int n) {
+        int sum = 0;
+        int n1=0;
+        int n2=1;
+        if(n == 0){
+            return 0;
+        } else if(n == 1){
+            return n1+n2;
+        } else {
+            for(int j=1;j<n;++j){
+                sum= n1+n2;
+                n1=n2;
+                n2=sum;
+            }
+        }
+        return sum;
     }
 }
