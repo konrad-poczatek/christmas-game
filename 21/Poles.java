e78bb74a7717fc72409b9a38516f0fc9a26d822e
arturo <arturo.rodriguez@leovegas.com>
2022-12-21 13:18:02 -0300

    21

--- 21/Poles.java
+++ 21/Poles.java
@@ -1,5 +1,14 @@
 class Poles {
     public int maxContainerArea(int[] height) {
-        
+        int maxHaight= 0;
+        int currentHight=0;
+        int min=0;
+        for (int i = 0; i < height.length-1; i++) {
+            for (int j = i+1, l=1; j < height.length; j++, l++) {
+                currentHight=Math.min(height[i], height[j])*l;
+                if (currentHight>maxHaight) maxHaight = currentHight;
+            }
+        }
+        return maxHaight;
     }
 }
