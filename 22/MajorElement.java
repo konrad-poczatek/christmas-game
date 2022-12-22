65d4f777e644a7cbed64aa1e1c944b8c86b642ad
arturo <arturo.rodriguez@leovegas.com>
2022-12-22 09:59:14 -0300

    22

--- 22/MajorElement.java
+++ 22/MajorElement.java
@@ -1,5 +1,21 @@
 class MajorElement {
     public int findMajorElement(int[] array) {
-        return 0;
+        int halfLengthPlusOne = array.length / 2 + 1;
+        int counter = 0;
+        int number=0;
+        boolean flag = false;
+        for (int i = 0; i < array.length; i++) {
+            number = array[i];
+            for (int j = 0; j < array.length; j++) {
+                if (number == array[j]) counter++;
+                if (counter == halfLengthPlusOne){
+                    flag = true;
+                    break;
+                }
+            }
+            if (flag) break;
+            counter = 0;
+        }
+        return number;
     }
 }
