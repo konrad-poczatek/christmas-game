64b48e5ab19f4feee2771b16550cd69a5bffc204
arturo <arturo.rodriguez@leovegas.com>
2022-12-23 10:15:29 -0300

    23

--- 23/Zeros.java
+++ 23/Zeros.java
@@ -1,5 +1,14 @@
 class Zeros {
     public void shiftZeroes(int[] array) {
-        
+        int temp;
+        for (int i = 0; i < array.length- 1 ; i++) {
+            for (int j = 0; j < array.length - i - 1; j++) {
+                if (array[j] < array[j+1]) {
+                    temp = array[j];
+                    array[j] = array[j+1];
+                    array[j+1] = temp;
+                }
+            }
+        }
     }
 }
