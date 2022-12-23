d7c1eb1cf526a7f8bca40393a9ac7c3f704cd490
andrey.yelmanov <andrey.yelmanov@leovegas.com>
2022-12-23 19:39:48 +0100

    Solution 23

--- 23/Zeros.java
+++ 23/Zeros.java
@@ -1,5 +1,17 @@
 class Zeros {
     public void shiftZeroes(int[] array) {
-        
+        int length = array.length-1;
+        for (int i = 0; i < array.length; i++) {
+            if(array[i] == 0){
+                moveLeft(array, i);
+                array[length] = 0;
+            }
+        }
+    }
+
+    private void moveLeft(int[] array, int start){
+        for (int i = start; i < array.length-1; i++) {
+            array[i] = array[i+1];
+        }
     }
 }
