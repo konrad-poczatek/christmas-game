1f8348ff9d4b3f05a4dfc36d5484e264e0f30e23
arturo <arturo.rodriguez@leovegas.com>
2022-12-24 10:05:36 -0300

    24

--- 24/ChristmasDinner.java
+++ 24/ChristmasDinner.java
@@ -1,5 +1,23 @@
 class ChristmasDinner {
     public int findSingle(int[] numbers) {
-        
+        int counter = 0;
+        int number=0;
+        boolean flag;
+        for (int i = 0; i < numbers.length; i++) {
+            number = numbers[i];
+            flag=true;
+            for (int j = 0; j < numbers.length; j++) {
+                if (number == numbers[j]) counter++;
+                if (counter == 2){
+                    flag = false;
+                    break;
+                }
+            }
+            if (flag){
+                break;
+            }
+            counter = 0;
+        }
+        return number;
     }
 }
