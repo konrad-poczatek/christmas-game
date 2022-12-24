1994616ebc3b47e505a9bd308a5ff9b9756eb3fc
andrey.yelmanov <andrey.yelmanov@leovegas.com>
2022-12-24 18:57:55 +0100

    Solution 24

--- 24/ChristmasDinner.java
+++ 24/ChristmasDinner.java
@@ -1,5 +1,24 @@
 class ChristmasDinner {
     public int findSingle(int[] numbers) {
-        
+        boolean hasNotPair = true;
+        List<Integer> hasPairs = new ArrayList<>();
+        for (int i = 0; i < numbers.length; i++) {
+            if (i == numbers.length - 1) {
+                return numbers[i];
+            }
+            for (int j = i + 1; j < numbers.length; j++) {
+                if (numbers[i] == numbers[j]) {
+                    hasNotPair = false;
+                    hasPairs.add(numbers[i]);
+                    break;
+                } else if (!hasPairs.contains(numbers[i])) {
+                    hasNotPair = true;
+                }
+            }
+            if (hasNotPair) {
+                return numbers[i];
+            }
+        }
+        return 0;
     }
 }
